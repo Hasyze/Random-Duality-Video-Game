09e73ce6e6420374d30f3efd577a1f8394cdad44
Noah Bompard <noah.bompard@etu.univ-grenoble-alpes.fr>
2022-06-09 10:30:02 +0200

    sprites

--- info3.game.given.2021/game/info3/game/Cowboy.java
+++ info3.game.given.2021/game/info3/game/Cowboy.java
@@ -42,7 +42,7 @@ public class Cowboy {
 	int y_speed;
 
 	Cowboy() throws IOException {
-		m_images = loadSprite("resources/winchester-4x6.png", 4, 6);
+		m_images = loadSprite("resources/walking_pilot4_resize.png", 4, 12);
 	}
 
 	/*

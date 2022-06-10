f9b25d355602c3081992924d1e8997992b6a20b5
Noah Bompard <noah.bompard@etu.univ-grenoble-alpes.fr>
2022-06-10 16:02:21 +0200

    séparation collision detectee et traitement des entitees dans EM

--- info3.game.given.2021/game/info3/game/Entity_Manager.java
+++ info3.game.given.2021/game/info3/game/Entity_Manager.java
@@ -18,10 +18,16 @@ public class Entity_Manager {
 	 }
 	 
 	 void EM_add(Entity obj) {
-		 entities[offset]=obj;
-		 offset++;
-		 
-	 }
+         if( obj instanceof DynamicEntity) {
+             Entities_Dynamic[offset_dynamic]=(DynamicEntity) obj;
+             offset_dynamic++;
+         }
+         /*else if ( obj instanceof StaticEntity){
+             entities_static[offset]=(StaticEntity) obj;
+             offset_static++;
+             }*/
+         
+     }
 	 
 	 void EM_remove(DynamicEntity obj){		 
 		int i = EM_find(obj);
@@ -36,7 +42,7 @@ public class Entity_Manager {
 		 offset_dynamic--;
 		//obj.detruire();
 	 }
-	 void EM_remove(StaticEntity obj) {
+	/* void EM_remove(StaticEntity obj) {
 		 int i = EM_find(obj);
 			if (i== -1) {
 				System.out.print("ERREUR : cannot find entitiy in entity_manager ! ");
@@ -48,19 +54,52 @@ public class Entity_Manager {
 			 Entities_Dynamic[offset_dynamic]= null;
 			 offset_dynamic--;
 			//obj.detruire();
-	 }
+	 }*/
 	 
-	 int EM_find(Entity obj) {
-		 for(int i =0; i< offset; i++) {
-			if (entities[i]== obj) { 
-				return i;
-				}
-		 	}
-		return -1;		 
-	 }
-	 
-	 void  eval() {
-		
-	 }
-	 
+	
+	
+ int EM_find(DynamicEntity obj) {
+         for(int i =0; i< offset_dynamic; i++) {
+            if (Entities_Dynamic[i]== obj) { 
+                return i;
+                }
+             }
+        return -1;         
+     }
+     /*int EM_find(StaticEntity obj) {
+         for(int i =0; i< offset_static; i++) {
+            if (entities_static[i]== obj) { 
+                return i;
+                }
+             }
+        return -1;         
+     }*/
+ 	Boolean collision_test(DynamicEntity Obj1, DynamicEntity Obj2) {
+ 		//TODO Aziz c'est ici les test de collision, tu renvoies true si oui il y a collision, false sinon
+ 		return false;
+ 	}
+ 
+ 	void eval() {
+ 		Entity_Manager L2 = new Entity_Manager();
+ 		
+ 		for(int i=0; i < offset_dynamic; i++) {
+ 			for (int j=0; j< offset_dynamic; j++) {
+ 				if (collision_test(this.Entities_Dynamic[i],this.Entities_Dynamic[j])) {
+ 					//
+ 				}
+ 			}
+ 		}
+ 		
+ 	}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 }

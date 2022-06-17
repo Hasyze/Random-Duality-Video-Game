e6af7c4b34b20f70baee500783fe8fa6324be3a3
Florian Machenaud <florian.machenaud@etu.univ-grenoble-alpes.fr>
2022-06-17 11:26:18 +0200

    chargement aléatoire background salles

--- info3.game.given.2021/game/Map/Salle.java
+++ info3.game.given.2021/game/Map/Salle.java
@@ -1,5 +1,39 @@
 package Map;
 
-public class Salle {
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
+import javax.imageio.ImageIO;
+
+public class Salle extends Etage{
+
+	Image background;
+	
+	
+	public Salle() {
+		
+	}
+	
+	public void init_background() throws IOException{
+		Image[] background = new Image[2];
+		background[0] = loadImage("resources/images_test/sprite_mur.png") ;
+		background[1] = loadImage("resources/images_test/sprite_mur2.png") ;
+		Random rand = new Random();
+		int r = rand.nextInt(2);
+		this.background = background[r];
+	}
+	
+	//Fonction pour récupérer une image
+	public static Image loadImage(String filename) throws IOException {
+		File imageFile = new File(filename);
+		if (imageFile.exists()) {
+			BufferedImage image = ImageIO.read(imageFile);
+			return image;
+		}
+		return null;
+	}
+	
 }
